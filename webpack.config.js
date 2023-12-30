379ec6defa2665af7646d460927cfbb3fd4aed1d
avtobys <avtobys.com@gmail.com>
2023-12-30 04:26:51 +0300

    PurgeCSSPlugin push only production

--- webpack.config.js
+++ webpack.config.js
@@ -22,121 +22,134 @@ const htmlPlugins = fs.readdirSync(htmlPagesPath).filter(file => {
   });
 })
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/main.js',
-  output: {
-    filename: 'assets/js/[contenthash].js',
-    chunkFilename: 'assets/js/[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true
-  },
-  plugins: [
+module.exports = (env, argv) => {
+  // Определение, запущена ли сборка в режиме production
+  const isProduction = argv.mode === 'production';
+
+  const plugins = [
     ...htmlPlugins,
     new miniCssExtractPlugin({
       filename: 'assets/css/[contenthash].css',
-    }),
-    new PurgeCSSPlugin({
-      paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`, { nodir: true }),
-      safelist: {
-        standard: [
-          // Bootstrap classes that might be added dynamically
-          'fade', 'show', 'collapsing', 'overflow',
-          // Modal related classes
-          'modal', 'modal-dialog', 'modal-open', 'modal-backdrop', 'offcanvas-backdrop',
-          // Dropdowns
-          'dropdown', 'dropdown-menu', 'dropdown-toggle',
-          // Popovers
-          'popover', 'popover-header', 'popover-body', 'bs-popover-auto', 'h3',
-          // Tooltips
-          'tooltip', 'tooltip-inner', 'arrow', 'bs-popover-top-arrow', 'bs-popover-right-arrow', 'bs-popover-bottom-arrow', 'bs-popover-left-arrow',
-          // Other
-          'carousel', 'carousel-inner', 'carousel-item',
-          // ... any other classes you want to keep
-        ],
-        deep: [
-          // Deep classes: regex pattern for classes that follow certain naming conventions
-          /^modal-/, /^carousel-/, /^tooltip-/, /^popover-/, /^dropdown-/, /^bs-popover-.*-arrow$/, /^bs-popover-/, /^offcanvas-/
-          // ... any other regex patterns for classes
-        ],
-        greedy: [
-          // Greedy classes: regex patterns for more complex class combinations
-          // ... any complex selectors
-        ],
-        keyframes: [
-          // Keyframes used in Bootstrap animations
-          'fade', 'collapse', 'modal', 'carousel',
-          // ... any other keyframes you want to keep
-        ],
-        variables: [
-          // CSS variables (custom properties) that Bootstrap might use
-          // ... any variables you want to keep
-        ]
-      },
     })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: miniCssExtractPlugin.loader
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: () => [
-                  autoprefixer
-                ]
-              }
-            }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name][ext]'
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimize: true, // Включает минимизацию
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false, // Удаляет комментарии
-          },
-        },
-        extractComments: false, // Не извлекать комментарии в отдельный файл
-      }),
-      new CssMinimizerPlugin({
-        minimizerOptions: {
-          preset: [
-            'default',
-            { discardComments: { removeAll: true } }, // Удаляет все комментарии из CSS
+  ];
+
+  // Добавление PurgeCSSPlugin только для продакшна
+  if (isProduction) {
+    plugins.push(
+      new PurgeCSSPlugin({
+        paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`, { nodir: true }),
+        safelist: {
+          standard: [
+            // Bootstrap classes that might be added dynamically
+            'fade', 'show', 'collapsing', 'overflow',
+            // Modal related classes
+            'modal', 'modal-dialog', 'modal-open', 'modal-backdrop', 'offcanvas-backdrop',
+            // Dropdowns
+            'dropdown', 'dropdown-menu', 'dropdown-toggle',
+            // Popovers
+            'popover', 'popover-header', 'popover-body', 'bs-popover-auto', 'h3',
+            // Tooltips
+            'tooltip', 'tooltip-inner', 'arrow', 'bs-popover-top-arrow', 'bs-popover-right-arrow', 'bs-popover-bottom-arrow', 'bs-popover-left-arrow',
+            // Other
+            'carousel', 'carousel-inner', 'carousel-item',
+            // ... any other classes you want to keep
           ],
+          deep: [
+            // Deep classes: regex pattern for classes that follow certain naming conventions
+            /^modal-/, /^carousel-/, /^tooltip-/, /^popover-/, /^dropdown-/, /^bs-popover-.*-arrow$/, /^bs-popover-/, /^offcanvas-/
+            // ... any other regex patterns for classes
+          ],
+          greedy: [
+            // Greedy classes: regex patterns for more complex class combinations
+            // ... any complex selectors
+          ],
+          keyframes: [
+            // Keyframes used in Bootstrap animations
+            'fade', 'collapse', 'modal', 'carousel',
+            // ... any other keyframes you want to keep
+          ],
+          variables: [
+            // CSS variables (custom properties) that Bootstrap might use
+            // ... any variables you want to keep
+          ]
         },
-      }),
-    ],
-  },
+      })
+    );
+  }
+
+  return {
+    mode: 'development',
+    entry: './src/js/main.js',
+    output: {
+      filename: 'assets/js/[contenthash].js',
+      chunkFilename: 'assets/js/[contenthash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      port: 8080,
+      hot: true
+    },
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.(scss)$/,
+          use: [
+            {
+              // Adds CSS to the DOM by injecting a `<style>` tag
+              loader: miniCssExtractPlugin.loader
+            },
+            {
+              // Interprets `@import` and `url()` like `import/require()` and will resolve them
+              loader: 'css-loader'
+            },
+            {
+              // Loader for webpack to process CSS with PostCSS
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: () => [
+                    autoprefixer
+                  ]
+                }
+              }
+            },
+            {
+              // Loads a SASS/SCSS file and compiles it to CSS
+              loader: 'sass-loader'
+            }
+          ]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/fonts/[name][ext]'
+          }
+        }
+      ]
+    },
+    optimization: {
+      minimize: true, // Включает минимизацию
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            format: {
+              comments: false, // Удаляет комментарии
+            },
+          },
+          extractComments: false, // Не извлекать комментарии в отдельный файл
+        }),
+        new CssMinimizerPlugin({
+          minimizerOptions: {
+            preset: [
+              'default',
+              { discardComments: { removeAll: true } }, // Удаляет все комментарии из CSS
+            ],
+          },
+        }),
+      ],
+    },
+  };
 }
